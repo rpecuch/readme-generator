6cd82a3d9b4aba5fe0ff32c40c8f30243dd0e0be
userrp <109092365+rpecuch@users.noreply.github.com>
2022-09-07 17:40:38 -0400

    wrote outline for functions to generate markdown and added questions to index file

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const markdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address:',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a description of your project:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information:',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license:',
+        name: 'license',
+        choices: ['MIT', 'Apache License 2.0', 'GPU GPLv3']
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer  
+        .prompt([
+            questions[0], questions[1], questions[2], questions[3], questions[4], questions[5]
+        ])
+        //now call function to write to README file
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === "MIT") {
+    //render mit badge
+  }
+  else if(license === "Apache License 2.0") {
+    //render badge
+  }
+  else if (license === "GPU GPLv3") {
+    //render badge
+  }
+  else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === "MIT") {
+    //render mit link
+  }
+  else if(license === "Apache License 2.0") {
+    //render link
+  }
+  else if (license === "GPU GPLv3") {
+    //render link
+  }
+  else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,6 +40,33 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 `;
+  renderLicenseBadge(data.license);
+  renderLicenseLink(data.license);
+  return `## Description\n\n${data.description}\n`;
+  return `## Table of Contents\n
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  `;
+  return `## Installation\n
+  Run the following command in the command line:
+    npm init
+  Run the following command in the command line:
+    npm i inquirer@8.2.4
+  `;
+  return `## Usage\n
+  ${data.usage}
+  `;
+  renderLicenseSection(data.license);
+  return `## Contributing\n
+  If you would like to contribute
+  `;
+  //tests section
+  return `## Questions\n
+  If you have any questions, do not hesitate to view my GitHub profile at [https://github.com/${data.username}](https://github.com/${data.username}) or contact me at ${data.email}.`
 }
 
 module.exports = generateMarkdown;
